0cb846cf7426e5f464db521a19960daeee2f47a3
caleressmisu <missun1201@me.com>
2025-06-07 15:32:53 +0900

    add chap5.1.1-3

---
+++ chap05/directive-twoway/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const inputName2way = ref('双方向');
+const onInputName2way = (event: Event) => {
+  const element = event.target as HTMLInputElement;
+  inputName2way.value = element.value;
+};
+</script>
+
+<template>
+  <section>
+    <input type="text" v-bind:value="inputName2way" v-on:input="onInputName2way"/>
+    <p>{{ inputName2way }}</p>
+  </section>
+</template>
