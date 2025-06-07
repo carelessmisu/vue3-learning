327ebad41bf9ba0e2c95941cb527dd7a0e7bf8de
caleressmisu <missun1201@me.com>
2025-06-07 14:42:22 +0900

    add chap4.2.3

---
+++ chap04/directive-on-param-event/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImgMouseMove = (event: MouseEvent) => {
+  mousePointerX.value = event.offsetX;
+  mousePointerY.value = event.offsetY;
+};
+</script>
+
+<template>
+  <section>
+    <img src="./assets/logo.svg" alt="Vue Logo"
+         width="200" v-on:mousemove="onImgMouseMove">
+    <p>Mouse Pointer Position: X: {{ mousePointerX }}, Y: {{ mousePointerY }}</p>
+  </section>
+</template>
