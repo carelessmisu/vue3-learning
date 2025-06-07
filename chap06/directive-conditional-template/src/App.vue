f181a386a452e171226244f8f5507d0a930b9516
caleressmisu <missun1201@me.com>
2025-06-07 16:44:56 +0900

    add chap6.1

---
+++ chap06/directive-conditional-template/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const randomNumber = computed(() => {
+  return Math.round(Math.random() * 100);
+});
+</script>
+
+<template>
+  <p>
+    点数は{{ randomNumber }}点で
+  <template v-if="randomNumber >= 80">
+    優です。
+    <span style="color:red">すばらしい!</span>
+  </template>
+  </p>
+</template>
---
+++ chap06/directive-show/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const number = ref(80);
+const showOrNot = computed(() => {
+  let showOrNot = false;
+  const rand = Math.round(Math.random() * 100);
+  if (rand >= 50) {
+    showOrNot = true;
+  }
+  return showOrNot;
+});
+</script>
+
+<template>
+  <section>
+    v-ifを利用
+    <p v-if="showOrNot">条件に合致したので表示</p>
+  </section>
+  <section>
+    v-showを利用
+    <p v-show="showOrNot">条件に合致したので表示</p>
+  </section>
+</template>
+
