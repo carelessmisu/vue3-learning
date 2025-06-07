788e015e92b395848de4ae25bf573005ed5fff5d
caleressmisu <missun1201@me.com>
2025-06-07 15:13:41 +0900

    add chap4.2.7

---
+++ chap04/directive-on-modifiers/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg = ref('まだです!');
+
+const onEnterKey =() => {
+  msg.value = 'Enterキーが押されました。';
+}
+
+const onRightButtonClick = () => {
+  msg.value = 'ボタンが右クリックされました。';
+}
+
+const onShiftClick = () => {
+  msg.value = 'Shiftキーが押しながらクリックされました。';
+}
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <input type="text" v-on:keydown.enter="onEnterKey"><br>
+  <button v-on:click.right="onRightButtonClick">右クリック</button><br>
+  <button v-on:click.shift="onShiftClick">Shiftを押しながらクリック</button><br>
+
+</template>
