858d6f7b3cda08c2795167e8d37336f8a4f8409f
caleressmisu <missun1201@me.com>
2025-06-14 14:29:14 +0900

    add chap6.2

---
+++ chap06/directive-loop-basic/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const cocktailListInit: string[] = [
+  'ホワイトレディ',
+  'ブルーハワイ',
+  'ニューヨーク',
+];
+const cocktailList = ref(cocktailListInit);
+</script>
+
+<template>
+  <ul>
+    <li v-for="cocktailName in cocktailList" v-bind:key="cocktailName">
+      {{ cocktailName }}
+    </li>
+  </ul>
+  <ul>
+    <li v-for="(cocktailName, index) in cocktailList" v-bind:key="cocktailName">
+      {{ cocktailName }}(インデックス{{ index }})
+    </li>
+  </ul>
+</template>
---
+++ chap06/directive-loop-obj/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const whiteladyInit: {
+  id: number,
+  name: string,
+  price: number,
+  recipe: string
+} = {
+  id: 2345,
+  name: 'ホワイトレディ',
+  price: 1200,
+  recipe: 'ジン30ml+コアントロー15ml+レモン果汁+15ml',
+};
+const whiteLady = ref(whiteladyInit);
+</script>
+
+<template>
+  <dl>
+    <template v-for="(value, key) in whiteLady"
+    v-bind:key="key">
+    <dt>{{ key }}</dt>
+    <dd>{{ value }}</dd>
+  </template>
+  </dl>
+</template>
