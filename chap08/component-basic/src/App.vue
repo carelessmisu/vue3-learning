df211e23180172f7afd883ec239c75dc95419b92
caleressmisu <missun1201@me.com>
2025-06-22 13:38:01 +0900

    add chap8.1

---
+++ chap08/component-basic/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import OneSection from './components/OneSection.vue';
+</script>
+
+<template>
+	<h1>Component Basics</h1>
+	<section>
+		<h2>One Component</h2>
+		<OneSection />
+	</section>
+	<section>
+		<h2>Multiple Component</h2>
+		<OneSection />
+		<OneSection />
+		<OneSection />
+	</section>
+</template>
+
+<style>
+section {
+	border: blue 1px solid;
+	margin: 10px;
+}
+</style>
