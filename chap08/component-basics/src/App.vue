e29463ae7dbeb4f3a06bec82389587f2d90ea9a8
caleressmisu <missun1201@me.com>
2025-06-22 14:43:47 +0900

    add chap8.3

---
+++ chap08/component-basics/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import OneInfo from './components/OneInfo.vue';
+</script>
+
+<template>
+	<h1>Props 基盤</h1>
+	<section>
+		<h2>属性に直接記述</h2>
+		<OneInfo title="Props の利用" content="子コンポーネントにデータを渡すには Props を利用する。"/>
+	</section>
+</template>
+
+<style>
+section {
+	border: blue 1px solid;
+	margin: 10px;
+}
+</style>
