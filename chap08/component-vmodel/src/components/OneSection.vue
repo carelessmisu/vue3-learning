c0910b5032c4bf8781edf45a1c13b53b8a6828ab
caleressmisu <missun1201@me.com>
2025-06-22 14:12:56 +0900

    add chap8.2

---
+++ chap08/component-vmodel/src/components/OneSection.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="box">
+        <h4>One Component</h4>
+        <p>What is a component?...</p>
+    </section>
+</template>
+
+<style>
+.box {
+    border: green 1px solid;
+    margin: 10px;
+}
+</style>
