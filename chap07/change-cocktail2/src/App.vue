ff2cc5ea8eff99c11a3f902afeec8e6ec794dce0
caleressmisu <missun1201@me.com>
2025-06-22 11:43:18 +0900

    add chap7.3

---
+++ chap07/change-cocktail2/src/App.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+
+interface Cocktail {
+  name: string;
+  price: number;
+}
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const cocktailDataListInit = new Map<number, Cocktail>();
+    cocktailDataListInit.set(1, { name: 'ホワイトレディ', price: 1200 });
+    cocktailDataListInit.set(2, { name: 'ブルーハワイ', price: 1500 });
+    cocktailDataListInit.set(3, { name: 'ニューヨーク', price: 1100 });
+    cocktailDataListInit.set(4, { name: 'マティーニ', price: 1500 });
+    const cocktailDataList = ref(cocktailDataListInit);
+
+    const cocktailNo = ref(1);
+    const priceMsg = computed(
+      (): string => {
+        const cocktail = cocktailDataList.value.get(cocktailNo.value);
+        let msg = "該当カクテルはありません。";
+        if (cocktail != undefined) {
+          msg = `該当するカクテルは${cocktail.name}で、値段は${cocktail.price}円です。`;
+        }
+        return msg;
+      }
+    );
+
+    setInterval(
+      (): void => {
+        cocktailNo.value = Math.round(Math.random() * 3) + 1;
+      }, 1000
+    )
+
+    return {
+      cocktailNo,
+      priceMsg
+    };
+  }
+});
+</script>
+
+<template>
+  <p>現在のカクテル番号: {{ cocktailNo }}</p>
+  <p>{{ priceMsg }}</p>
+</template>
