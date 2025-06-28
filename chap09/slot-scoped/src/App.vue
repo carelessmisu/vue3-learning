eb7744daccd17f19f71ac851aa2bca1bc3ff1759
caleressmisu <missun1201@me.com>
2025-06-28 10:33:16 +0900

    add chap9.3

---
+++ chap09/slot-scoped/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import OneSection from './components/OneSection.vue';
+
+</script>
+
+<template>
+	<section>
+		<OneSection>
+			<template v-slot:default="slotProps">
+				<dl>
+					<dt>スロットの名前</dt>
+					<dd>{{ slotProps.memberInfo.name }}</dd>
+					<dt>スロットの値</dt>
+					<dd>{{ slotProps.memberInfo.state }}</dd>
+				</dl>
+			</template>
+		</OneSection>
+	</section>
+</template>
---
+++ chap09/slot-scoped/src/components/OneSection.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const memberInfo = reactive({
+    name: '山田太郎',
+    state: '問題ありません。'
+})
+</script>
+
+<template>
+    <section>
+        <slot v-bind:memberInfo="memberInfo">
+            <h1>{{ memberInfo.name }}さんの状況</h1>
+            <p>{{ memberInfo.state }}</p>
+        </slot>
+    </section>
+</template>
+
+<style>
+.box {
+    border: green 1px solid;
+    margin: 10px;
+}
+</style>
